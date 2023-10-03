f81aa03120113ca5e2199b8c0c7a2a54df89ece7
annascriv <scriven.anna@outlook.com>
2023-10-03 16:37:19 -0500

    added another print function

--- annas.js
+++ annas.js
@@ -1,3 +1,15 @@
 function addEmUp(num1,num2) {
     return num1+num2
 }
+
+function printAlphabet(str) {
+    str=str.split('');
+    let alphabet='abcdefghijklmnopqrstuvwxyz'
+    let newStr='';
+
+    for (let i=0;i<str.length;i++) {
+        if (alphabet.includes(str[i])) {
+            newStr+=str[i]
+        } return newStr
+    }
+}
